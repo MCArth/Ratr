a4b6f46d597f79328bd4b74e6d6eba5488f8f3dd
Yassin Ouzzane <youzzane@gmail.com>
2020-02-19 22:23:04 +0000

    Build method for override

--- Frontend/lib/list.dart
+++ Frontend/lib/list.dart
@@ -5,6 +5,9 @@ import 'package:flutter/material.dart';
 //required to display only a small amount of information that nevertheless expresses the general sense for a property at a glance
 //Maybe use tiles to display an image + price per month + an address
 class ListPage extends StatelessWidget{
-
-
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
 }
--- Frontend/lib/main.dart
+++ Frontend/lib/main.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
 import 'app.dart';
 
-
 void main() => runApp(NexusApp());
-
-
