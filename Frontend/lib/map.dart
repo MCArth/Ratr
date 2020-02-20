bb7a4c75078e761c96f4828ceab02b26c77524f1
Infracanine <Samueltaylorhansen@gmail.com>
2020-02-20 12:48:10 +0000

    adding basic map screen

--- Frontend/lib/map.dart
+++ Frontend/lib/map.dart
@@ -1,15 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'dart:async';
 
-class MapPage extends StatelessWidget{
-@override
-	Widget build(BuildContext context) {
-		return Scaffold(
-			appBar: AppBar(
-				title: Text('Map Page'),
-			),
-			body: Center(
-			),
-		);
-	}
+class MapPage extends StatefulWidget{ 
+  @override
+  _MapState createState() => _MapState();
+}
+
+class _MapState extends State<MapPage>{
+  Completer<GoogleMapController> _controller = Completer();
+
+  static const LatLng _center = const LatLng(51.3782, -2.3264);
+
+  void _onMapCreated(GoogleMapController controller) {
+    _controller.complete(controller);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text('Properties in your area'),
+        ),
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 11.0,
+          ),
+    ),
+    );
+  }
 }
