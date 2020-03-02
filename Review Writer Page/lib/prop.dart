52e22c73a35ca133f14954d39850e4addae50c38
Yassin Ouzzane <youzzane@gmail.com>
2020-03-02 02:38:30 +0000

    Changed class names, a few edits to property profile

--- Review Writer Page/lib/prop.dart
+++ Review Writer Page/lib/prop.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/main.dart';
+import 'package:flutter_app/review.dart';
 
 void main() => runApp(Property());
 
@@ -15,7 +15,7 @@ class Property extends StatelessWidget {
 }
 
 class PropertyProfile extends StatelessWidget {
-  //todo change this
+  //todo change this, not propName anymore
   final String propertyName = "Property Profile";
 
   @override
@@ -40,45 +40,72 @@ class PropertyProfile extends StatelessWidget {
                 children: <Widget>[
                   Padding(
                     padding:
-                        EdgeInsets.symmetric(horizontal: 1.0, vertical: 12.0),
+                        EdgeInsets.symmetric(horizontal: 1.0, vertical: 1.0),
                   ),
                   Container(
                       child: Icon(
-                    Icons.home,
-                    size: 80,
+                    //todo property image goes here
+                    Icons.image,
+                    size: 110,
                   )),
-                  SizedBox(width: 50.0),
-                  Column(children: <Widget>[
-                    Container(
-                      child: Text(
-                          //todo this is just a place holder
-                          '5 Green Street',
-                          style: TextStyle(
-                              fontSize: 18, fontWeight: FontWeight.bold),
-                          textAlign: TextAlign.left),
-                    ),
-                    Container(
-                      child: Text(
-                          //todo this is just a place holder
-                          'BA2 6FP',
-                          style: TextStyle(
-                              fontSize: 16, fontStyle: FontStyle.italic),
-                          textAlign: TextAlign.left),
-                    ),
-                    Container(
-                      child: Text('Last Known Landlord:',
-                          style: TextStyle(
-                              color: Colors.grey,
-                              fontSize: 16,
-                              fontStyle: FontStyle.italic),
-                          textAlign: TextAlign.left),
-                    )
-                  ])
+                  SizedBox(width: 30.0),
+                  Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Container(
+                          child: Text(
+                              //todo this is just a place holder
+                              '5 Green Street',
+                              style: TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.bold),
+                              textAlign: TextAlign.left),
+                        ),
+                        Container(
+                            child: Text(
+                                //todo this is just a place holder
+                                'BA2 6FP',
+                                style: TextStyle(
+                                    fontSize: 16, fontStyle: FontStyle.italic),
+                                textAlign: TextAlign.left)),
+                        Container(
+                          child: Text('Last Known Landlord:',
+                              style: TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 16,
+                                  fontStyle: FontStyle.italic),
+                              textAlign: TextAlign.left),
+                        ),
+                        Container(
+                          child: Text('Roger Mexico',
+                              style: TextStyle(
+                                  color: Colors.red,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold),
+                              textAlign: TextAlign.left),
+                        ),
+                        Container(
+                          child: Text('Last Known Agency:',
+                              style: TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 16,
+                                  fontStyle: FontStyle.italic),
+                              textAlign: TextAlign.left),
+                        ),
+                        Container(
+                          //todo get rid of hardcoding
+                          child: Text('Trustease',
+                              style: TextStyle(
+                                  color: Colors.red,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold),
+                              textAlign: TextAlign.left),
+                        )
+                      ])
                 ],
               )
             ],
           ),
-          SizedBox(height: 20),
+          SizedBox(height: 35),
           Container(
             padding: EdgeInsets.symmetric(horizontal: 2.0, vertical: 5),
             child: Align(
@@ -90,20 +117,26 @@ class PropertyProfile extends StatelessWidget {
                 )),
           ),
           Container(
-            padding: EdgeInsets.symmetric(vertical: 10),
-            child: TextField(
-                keyboardType: TextInputType.multiline,
-                minLines: 4,
-                maxLines: 4,
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(3.0),
-                        borderSide:
-                            BorderSide(width: 10.0, style: BorderStyle.solid)),
-                    filled: true,
-                    hintText: 'Stuff goes here....')
-                //labelText: 'House Review')
-                ),
+            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 2),
+            //todo backend stuff here
+            child: Text('No information yet.'),
+            decoration: BoxDecoration(
+              border: Border.all(),
+              shape: BoxShape.rectangle
+            ),
+//            child: TextField(
+//                keyboardType: TextInputType.multiline,
+//                minLines: 4,
+//                maxLines: 4,
+//                decoration: InputDecoration(
+//                    border: OutlineInputBorder(
+//                        borderRadius: BorderRadius.circular(3.0),
+//                        borderSide:
+//                            BorderSide(width: 10.0, style: BorderStyle.solid)),
+//                    filled: true,
+//                    hintText: 'Stuff goes here....')
+//                //labelText: 'House Review')
+//                ),
           ),
           SizedBox(height: 20.0),
           Container(
