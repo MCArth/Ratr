871a28a112cc3063c81736900ecb89e9ce3590f9
Yassin Ouzzane <youzzane@gmail.com>
2020-03-02 13:18:03 +0000

    THE YAML IS FUCKED

--- Frontend/lib/registration.dart
+++ Frontend/lib/registration.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:string_validator/string_validator.dart';
+//import 'package:string_validator/string_validator.dart';
 import 'dart:developer';
 
 class RegistrationPage extends StatefulWidget {
