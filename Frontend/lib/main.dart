2f1006d97295650eb4d2c617535c45b7ea820e4a
Infracanine <Samueltaylorhansen@gmail.com>
2020-03-02 12:31:33 +0000

    Hoping to god API keys are properly ignored

--- Frontend/lib/main.dart
+++ Frontend/lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'app.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
+Future main() async {
+  await DotEnv().load('auth.env');
+  runApp(NexusApp());
+  }
 
-void main() => runApp(NexusApp());
 
 
