12d2e7a50eddc0d98599bbd3a68d1cd2fdb17d5c
Infracanine <Samueltaylorhansen@gmail.com>
2020-03-04 15:47:28 +0000

    more api key nonsense

--- Frontend/ios/Runner/AppDelegate.swift
+++ Frontend/ios/Runner/AppDelegate.swift
@@ -7,7 +7,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("APIKEY")
+    GMSServices.provideAPIKey("TEST")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
