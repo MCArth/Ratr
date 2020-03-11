cebfdfccf4424878d8d70c47926838f89f37574a
Alex-apostolo <alex-apostolo@hotmail.com>
2020-03-11 17:46:52 +0000

    another commit

--- Frontend/lib/list.dart
+++ Frontend/lib/list.dart
@@ -10,17 +10,58 @@ class ListPage extends StatelessWidget{
     return Scaffold(
       appBar: new AppBar(
         title: Text("List View")),
-        body: Padding(
-          padding: EdgeInsets.fromLTRB(0,10.0,0,10.0),
-          child: getListViewBody(context)
-        )
-
-      );
+        body: makeHouseCard,
+        );
   }
-
-
 }
 
-getListViewBody(BuildContext context){
+final makeHouseCard = Card(
+  elevation: 8.0,
+  margin: new EdgeInsets.symmetric(
+    horizontal: 10.0,
+    vertical: 6.0,
+  ),
+  child: Container(
+    decoration: BoxDecoration(),
+    child: makeCardStructure,
+    ),
+);
 
-}
+final makeCardStructure = ListTile(
+        contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+        leading: Container(
+          padding: EdgeInsets.only(right: 12.0),
+          decoration: new BoxDecoration(
+              border: new Border(
+                  right: new BorderSide(width: 1.0, color: Colors.white24))),
+          child: Icon(Icons.autorenew, color: Colors.white),
+        ),
+        title: Text(
+          "House",
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        // subtitle: Text("Intermediate", style: TextStyle(color: Colors.white)),
+
+        subtitle: Row(
+          children: <Widget>[
+            Icon(Icons.linear_scale, color: Colors.yellowAccent),
+            Text(" Intermediate", style: TextStyle(color: Colors.white))
+          ],
+        ),
+        trailing:
+            Icon(Icons.keyboard_arrow_right, color: Colors.white, size: 30.0)
+);
+
+//Function that gets all houses from database, creates card for each one
+final getListViewBody = Container(
+  child: ListView.builder(
+    scrollDirection: Axis.vertical,
+    shrinkWrap: true,
+    //TODO make this dynamically size house directory 
+    itemCount: 10,
+    itemBuilder: (BuildContext context, int index){
+      return makeHouseCard;
+    },
+  )
+);
+
